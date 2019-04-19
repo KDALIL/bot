328309e415a1754b5f550638ff8c465b84e03638
jmdev <jmorassin@gmail.com>
2019-04-19 11:50:58 +0200

    Ajout du scrupt BD

---
+++ BOT_BITCOIN_BINANCE_JAVA/database/creation.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bot;
+
+
+CREATE TABLE `bot`.`cours` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `symbol` VARCHAR(15) NULL,
+  `volume` INT NULL,
+  `prix` DECIMAL(10,10) NULL,
+  `dateEnregistrement` DATETIME NULL,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  PRIMARY KEY (`id`))
+COMMENT = 'Cours des cryptos';
